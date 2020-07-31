b5d9fe4c599c11e8db88d1725b23ae8a0998f46c
jorditoda <jordicatalunya@hotmail.com>
2020-07-31 16:21:53 +0200

    Train done

--- lib/pages/activity_finished.dart
+++ lib/pages/activity_finished.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:esys_flutter_share/esys_flutter_share.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:screenshot/screenshot.dart';
 import 'package:workball/components/line_text.dart';
 import 'package:workball/components/summary_row.dart';
 import 'package:workball/tabs/tabs.dart';
@@ -24,82 +28,114 @@ class _ActivityFinishedState extends State<ActivityFinished> {
   int workingTime;
   int nExercises;
   String difficulty;
+  File _imageFile;
+
+  //Create an instance of ScreenshotController
+  ScreenshotController screenshotController = ScreenshotController();
 
   _ActivityFinishedState(this.workingTime, this.difficulty, this.nExercises);
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     double space = (MediaQuery.of(context).size.height - 660);
 
-    return Scaffold(
-      backgroundColor: Constants.blackBackground,
-      appBar: AppBar(
+    return Screenshot(
+      controller: screenshotController,
+      child: Scaffold(
         backgroundColor: Constants.blackBackground,
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        title: Header(
-          title: 'WB',
-        ),
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: Icon(Icons.share, color: Colors.white),
+        appBar: AppBar(
+          backgroundColor: Constants.blackBackground,
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: Header(
+            title: 'WB',
           ),
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          LineText(
-            Text(
-              "RESUMEN",
-              style: TextStyle(
-                  fontSize: 24,
-                  fontFamily: "Impact",
-                  color: Constants.oddYellow),
-            ),
-          ),
-          Padding(
-//            padding: const EdgeInsets.only(top: 30, bottom: 30),
-            padding: const EdgeInsets.only(top: 0),
-            child: Text(
-              difficulty,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 27,
-                color: Colors.white,
-                fontFamily: "Impact",
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: IconButton(
+                icon: Icon(Icons.share, color: Colors.white),
+                onPressed: shareAndScreenshot,
               ),
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 150,
-            child: Image.asset(
-              "assets/images/champion.png",
-              fit: BoxFit.fitHeight,
-            ),
-          ),
-          SummaryRow(nExercises.toString(), "256", workingTime),
-          _getCentralButton(),
-          SizedBox(height: space > 10 ? 10 : space),
-        ],
-      ),
-      bottomNavigationBar: GestureDetector(
-        child: CustomButtonWidget(
-          'FINALIZAR ENTRENAMIENTO',
-          textColor: Colors.white,
+          ],
         ),
-        onTap: () {
-          Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(builder: (context) => Tabs("", "")),
-              (Route<dynamic> route) => false);
-        },
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            LineText(
+              Text(
+                "RESUMEN",
+                style: TextStyle(
+                    fontSize: 24,
+                    fontFamily: "Impact",
+                    color: Constants.oddYellow),
+              ),
+            ),
+            Padding(
+//            padding: const EdgeInsets.only(top: 30, bottom: 30),
+              padding: const EdgeInsets.only(top: 0),
+              child: Text(
+                difficulty,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 27,
+                  color: Colors.white,
+                  fontFamily: "Impact",
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              child: Image.asset(
+                "assets/images/champion.png",
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            SummaryRow(nExercises.toString(), "256", workingTime),
+            _getCentralButton(),
+            SizedBox(height: space > 10 ? 10 : space),
+          ],
+        ),
+        bottomNavigationBar: GestureDetector(
+          child: CustomButtonWidget(
+            'FINALIZAR ENTRENAMIENTO',
+            textColor: Colors.white,
+          ),
+          onTap: () {
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => Tabs("", "")),
+                (Route<dynamic> route) => false);
+          },
+        ),
       ),
     );
   }
 
+//
+  void shareAndScreenshot() async {
+    screenshotController.capture().then((File image) {
+      //Capture Done
+      setState(() {
+        _imageFile = image;
+
+        Share.file('Entrenamiento', '${DateTime.now()}.png',
+            _imageFile.readAsBytesSync(), 'image/png');
+      });
+    }).catchError((onError) {
+      print(onError);
+    });
+
+  }
+
   Widget _getCentralButton() {
+    //TODO: restart train
     return Stack(
       children: <Widget>[
         Column(
